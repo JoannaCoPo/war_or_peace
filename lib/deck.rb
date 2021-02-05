3fab4701dac10fd6a2c2b9645b0c698600d7beb6
JoannaCoPo <jococopo@gmail.com>
2021-02-04 20:13:04 -0700

    Initial commit

---
+++ lib/deck.rb
@@ -0,0 +1,27 @@
+class Deck
+  attr_reader :cards
+# ^^only for IV's
+
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def rank_of_card_at(index)
+    single_card = @cards[index] #generic
+    single_card.rank
+  end
+
+  def high_ranking_cards # return an array of cards in the deck that have a rank of 11+
+    high_cards = @cards
+    high_cards.find_all do |card|
+      card.rank >= 11
+    end
+  end
+
+  def percent_high_ranking
+    high_cards = @cards.find_all do |card|
+      card.rank >= 11
+    end
+    high_cards.count.to_f / @cards.count.to_f
+  end
+end
