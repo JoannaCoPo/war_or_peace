9520f6b4b7289486e2f3ef997f74f9c156740aa1
JoannaCoPo <jococopo@gmail.com>
2021-02-04 20:39:42 -0700

    Update

--- test/card_test.rb
+++ test/card_test.rb
@@ -1,19 +1,29 @@
+
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
+require 'pry'
+
 
 class CardTest < Minitest::Test
   def test_it_exists
     card = Card.new(:diamond, 'Queen', 12)
-
     assert_instance_of Card, card
   end
 
   def test_it_has_readable_attributes
     card = Card.new(:diamond, 'Queen', 12)
-    
     assert_equal :diamond, card.suit
     assert_equal 'Queen', card.value
     assert_equal 12, card.rank
+  end
+
+  def test_it_has_new_cards
+    # skip
+    card = Card.new(:heart, 'Jack', 11)
+    assert_equal :heart, card.suit
+    assert_equal 'Jack', card.value
+    assert_equal 11, card.rank
+    binding.pry
   end
 end
