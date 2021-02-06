acf6229feac86fd59b4d128e9adc32442f3dd821
JoannaCoPo <jococopo@gmail.com>
2021-02-06 14:33:27 -0700

    Add through 'winner'

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,12 +4,10 @@ class Turn
               :spoils_of_war
 
 
-
-
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
-    @spoils_of_war
+    @spoils_of_war = []
   end
 
   def player1_t
@@ -20,9 +18,21 @@ class Turn
     player2
   end
 
+  def type
+    if player1_t.deck.rank_of_card_at(0) != player2_t.deck.rank_of_card_at(0)
+      :basic
+    end
+  end
 
+
+  def turn_winner
+    card1 = player1_t.remove_card
+    card2 = player2_t.remove_card
+    # binding.pry
+    if card1.rank > card2.rank
+      player1
+    else
+      player2
+    end
+  end
 end
-
-#
-# player1.deck.remove_card
-# player1.name
