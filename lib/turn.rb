614883aae17c7990e6d013c0c209a9be9c5b2574
JoannaCoPo <jococopo@gmail.com>
2021-02-06 10:53:29 -0700

    Create turn.rb

---
+++ lib/turn.rb
@@ -0,0 +1,28 @@
+class Turn
+  attr_reader :player1,
+              :player2,
+              :spoils_of_war
+
+
+
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @spoils_of_war
+  end
+
+  def player1_t
+    player1
+  end
+
+  def player2_t
+    player2
+  end
+
+
+end
+
+#
+# player1.deck.remove_card
+# player1.name
