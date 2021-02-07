131f15f9224fc123fd0ec3c7f20ea2c50c67b9f8
JoannaCoPo <jococopo@gmail.com>
2021-02-07 07:16:15 -0700

    Add test for 'mutually_assured_destruction'

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -24,7 +24,7 @@ class TurnTest < Minitest::Test
     assert_instance_of Turn, turn
   end
 
-  def test_it_has_readable_attributes
+  def test_it_has_basic_turns
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
@@ -38,13 +38,48 @@ class TurnTest < Minitest::Test
     player1 = Player.new("Megan", deck1)
     player2 = Player.new("Aurora", deck2)
     turn = Turn.new(player1, player2)
-    assert_equal player1, turn.player1_t
-    assert_equal player2, turn.player2_t
+    assert_equal player1, turn.player1_turn
+    assert_equal player2, turn.player2_turn
     assert_equal [], turn.spoils_of_war
-    # binding.pry
     assert_equal :basic, turn.type
-    assert_equal player1, turn.turn_winner
+    assert_equal player1, turn.winner
+    # assert_equal card1, turn.spoils_of_war
+    assert_equal card1, turn.pile_cards
+    # assert_equal card8, player1.deck
+    # ssert_equal card7, card1,card2, card5, card4, card3, card6,
+    # player2.deck
   end
 
+
+
+#:mutually_assured_destruction occurs when both players’
+#rank_of_card_at(0) AND rank_of_card_at(2) are the same.
+#if the turn has a type of :mutually_assured_destruction
+#the method will return No Winner.
+
+  def test_it_has_mutually_assured_destruction
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    assert_equal player1, turn.player1_turn
+    assert_equal player2, turn.player2_turn
+    assert_equal :mutually_assured_destruction, turn.type
+
   end
-# end
+
+
+end
+
+
+
+#
