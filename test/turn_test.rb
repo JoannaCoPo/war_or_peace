7077c5a263b641784abb6f5d37ec9430ce419a2b
JoannaCoPo <jococopo@gmail.com>
2021-02-07 16:52:16 -0700

    Fix war_test

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -58,13 +58,18 @@ class TurnTest < Minitest::Test
   def test_it_has_war_turns
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
-    card3 = Card.new(:heart, '9', 9)
+    card3 = Card.new(:heart, '9', 9 )
     card4 = Card.new(:diamond, 'Jack', 11)
-    deck1 = Deck.new([card1, card2])
-    deck2 = Deck.new([card3, card4])
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 3)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
     player1 = Player.new("Megan", deck1)
     player2 = Player.new("Aurora", deck2)
     turn = Turn.new(player1, player2)
+    # binding.pry
     assert_equal :war, turn.type
     # turn.pile_cards
     # winner = turn.winner
