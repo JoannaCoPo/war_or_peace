d1f94e713cd46cb9814ef3b96c9610034cb36575
JoannaCoPo <jococopo@gmail.com>
2021-02-07 07:17:10 -0700

    Add method changes

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,29 +10,46 @@ class Turn
     @spoils_of_war = []
   end
 
-  def player1_t
+  def player1_turn
     player1
   end
 
-  def player2_t
+  def player2_turn
     player2
   end
 
   def type
-    if player1_t.deck.rank_of_card_at(0) != player2_t.deck.rank_of_card_at(0)
+    if player1_turn.deck.rank_of_card_at(0) != player2_turn.deck.rank_of_card_at(0)
       :basic
     end
   end
 
 
-  def turn_winner
-    card1 = player1_t.remove_card
-    card2 = player2_t.remove_card
-    # binding.pry
-    if card1.rank > card2.rank
+  def winner #issue here - check state of object
+    @top_card1 = player1.deck.remove_card
+    @top_card2 = player2.deck.remove_card
+
+    if @top_card1.rank > @top_card2.rank
       player1
     else
       player2
     end
   end
+
+  def pile_cards
+    #pile_cards: when this method is called, cards will be
+    #sent from the players’ decks into the @spoils_of_war
+    #based on these rules for a :basic turn, each player
+    #will send one card (the top card) to the spoils pile
+    #for a :mutually_assured_destruction turn, each player will remove three
+    #cards from play (the top three cards in their deck). These cards are not
+    #sent to the spoils pile, they are simply removed from each players’ deck.
+    if mutually_assured_destruction
+    @top_card1 = player1.deck.remove_card
+    # top_card2 = player2_t.remove_card
+    spoils_of_war << @top_card1
+    # biding.pry
+    # spoils_of_war << top_card1
+  end
+
 end
